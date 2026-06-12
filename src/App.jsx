aae0290dafa557feffee8e1d062e32a392eb4a93
Shivam Sharma <shivam.sharma@leverageedu.com>
2026-06-13 00:58:43 +0530

    App: wrap with ThemeProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react'
 import { Routes, Route, Navigate, useLocation } from 'react-router-dom'
+import { ThemeProvider } from './context/ThemeContext'
 import { useAuth } from './hooks/useAuth'
 import { logActivity } from './components/ActivityLogger'
 import LoginPage from './pages/LoginPage'
@@ -53,6 +54,7 @@ function ProtectedRoute({ children, dashboardId }) {
 
 export default function App() {
   return (
+    <ThemeProvider>
     <Routes>
       <Route path="/login" element={<LoginPage />} />
       <Route path="/"                       element={<ProtectedRoute dashboardId="home">        <DashboardHome /></ProtectedRoute>} />
@@ -69,5 +71,6 @@ export default function App() {
       <Route path="/settings"               element={<ProtectedRoute dashboardId="settings">    <SettingsPage /></ProtectedRoute>} />
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
+    </ThemeProvider>
   )
 }
